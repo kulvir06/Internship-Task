4064e9d67448e1c2ad752e3720aa0228f77a4a8c
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 22:18:42 +0530

    Update emailExtractor.js
    simplified the function using arrow functions and split method

--- user methods/emailExtractor.js
+++ user methods/emailExtractor.js
@@ -1,18 +1,3 @@
 module.exports={
-    email: function(str){
-        var emails = [];
-        var w="";    
-        str = str+",";
-        var len = str.length;
-        for(i=0;i<len;i++){
-            if(str[i]===","){
-                emails.push(w);
-                w="";
-            } 
-            else {
-                w=w+str[i];
-            } 
-        }
-        return emails;   
-    }
+    email: (str) => str.split(",")     
 }
