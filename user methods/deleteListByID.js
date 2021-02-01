a6e9b91989d528df8fce98ed28d09da7fefb29e6
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 23:19:56 +0530

    Update deleteListByID.js

--- user methods/deleteListByID.js
+++ user methods/deleteListByID.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
 const dbconnect = require("./connection.js");
-var con =dbconnect.connection();
+const con =dbconnect.connection();
 
 module.exports={
-    delete : function(lists){
-        for(var i = 0;i<lists.length;i++){
-            var sql = "delete from lists where id = "+mysql.escape(lists[i]);
+    delete : (lists) => {
+        lists.forEach(list => {
+            let sql = "delete from lists where id = "+mysql.escape(list);
             con.query(sql,function(err,result,fields){
                 if(err) throw err;
                 console.log("deleted lists");
             })
-        }
+                        
+        });
     }
 }
