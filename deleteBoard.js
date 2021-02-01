197e9753822e4c4b3be429b2641119c0426f26ca
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 22:28:40 +0530

    Update deleteBoard.js
    used let in place of var

--- deleteBoard.js
+++ deleteBoard.js
@@ -5,24 +5,27 @@ const emailExtractor = require("./user methods/emailExtractor.js");
 const deleteListByID = require("./user methods/deleteListByID.js");
 
 const dbconnect = require("./user methods/connection.js");
-var con =dbconnect.connection();
+const con =dbconnect.connection();
 
 router.get("/:id",function(req,res){
-    var name = req.params.id;
-    var sql = "select lists from boards where name = "+mysql.escape(name)+" limit 1 ;"
+
+    let name = req.params.id;
+    const sql = "select lists from boards where name = "+mysql.escape(name)+" limit 1 ;"
+
     con.query(sql,function(err,result,fields){
         if(err) throw err;
-        var listID = (result[0].lists);
+        let listID = (result[0].lists);
         listID = listID.substring(0,listID.length-1);
-        var lists = (emailExtractor.email(listID));
+        let lists = (emailExtractor.email(listID));
         deleteListByID.delete(lists);     
     })
+
     sql = "delete from boards where name = "+mysql.escape(name)+";";
     con.query(sql,function(err,result,fields){
         if(err) throw err;
         console.log('deleted board');
+        
     })
-
 })
 
 module.exports = router;
