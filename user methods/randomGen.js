0c39ad81a98e14ab11f88184cb898b76b3e4bab2
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 21:59:32 +0530

    Update randomGen.js
    added find array helper method, cleaned redundant code

--- user methods/randomGen.js
+++ user methods/randomGen.js
@@ -1,20 +1,14 @@
 
 const boardIDs = [];
 module.exports={
-    randomGen: function(){
-        var finalID;
-    while(true){ 
-        var idGen = Math.random();
-        idGen = idGen.toString();
-        idGen = idGen.substring(0,7);
-        if(boardIDs.includes(idGen)===true) console.log("Retry ID Generation");
-        else{
-            boardIDs.push(idGen)
-            finalID=idGen;
-            //  console.log(boardIDs);
-            break;
+    randomGen: () => {        
+        while(true){
+
+            const newRandomNumber = Math.random().toString().substring(0,7);
+            const value = boardIDs.find((boardID) => boardID===newRandomNumber );
+
+            if(typeof value != "undefined"  ) console.log("Retry Id Generation");
+            else return newRandomNumber; 
         }
-    }
-    return finalID;
     }
 }
