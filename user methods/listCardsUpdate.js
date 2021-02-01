edc2565b53874c137fa19da4cc1bb130a57a6d2b
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 23:21:01 +0530

    Update listCardsUpdate.js

--- user methods/listCardsUpdate.js
+++ user methods/listCardsUpdate.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 const dbconnect = require("./connection.js");
 
-var con =dbconnect.connection();
+const con =dbconnect.connection();
 
 module.exports={
-    update: function(cards,listName,id){
-        var sql = "update lists set cards = "+mysql.escape(cards)+"where name = "+mysql.escape(listName)+" and id=" +mysql.escape(id)+";";
+    update: (cards,listName,id) => {
+        let sql = "update lists set cards = "+mysql.escape(cards)+"where name = "+mysql.escape(listName)+" and id=" +mysql.escape(id)+";";
         con.query(sql,function(err,result,fields){
             if(err) throw err;
             
