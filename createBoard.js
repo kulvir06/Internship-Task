18bacf4b53a1a86aba8bda85d920f763e33ee0fb
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 22:24:51 +0530

    Update createBoard.js
    used forEach

--- createBoard.js
+++ createBoard.js
@@ -1,83 +1,40 @@
 const express = require("express");
-const router = express.Router();
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const router = express.Router();
+
 const randomGen = require('./user methods/randomGen');
 const emailExtractor = require("./user methods/emailExtractor.js");
-const mysql = require('mysql');
 
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(express.json());
 
 const dbconnect = require("./user methods/connection.js");
-var con =dbconnect.connection();
 
+let con =dbconnect.connection();
 
 router.get("/",function(req,res){
+
     res.sendFile(__dirname+"/html/createBoard.html")
+
 })
 
 router.post("/",function(req,res){
-    var name = req.body.name;
-    var members = req.body.name1;
-    var id = randomGen.randomGen();
-    id = id.toString();
-    var email = (emailExtractor.email(members));
-    var len = email.length;
-    for(var i=0;i<len;i++){
-        var sql = "insert into boards values ("+mysql.escape(id)+","+mysql.escape(name)+","+mysql.escape(email[i])+",null);";
+
+    let name = req.body.name;
+    let members = req.body.name1;
+    let id = randomGen.randomGen().toString();
+
+    let emails = (emailExtractor.email(members));
+    emails.forEach(email => {
+        let sql = "insert into boards values ("+mysql.escape(id)+","+mysql.escape(name)+","+mysql.escape(email)+",null);";
         con.query(sql,function(err,result,fields){
             if(err) throw err;
-        })
-    }
+        })        
+    });
     console.log("Created a Board");
 
 })
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function resolveAfter2Seconds() {   
-//     return new Promise(resolve => {        
-//       setTimeout(() => {
-
-//         var finalID;
-//         while(true){ 
-//             var idGen = Math.random();
-//             if(boardIDs.includes(idGen)===true) console.log("Retry ID Generation");
-//             else{
-//                 boardIDs.push(idGen)
-//                 finalID=idGen;
-//                 console.log(boardIDs);
-//                 break;
-//             }
-//         }
-//         resolve(finalID);
-//       }, 2000);
-//     });
-//   }
-
-// async function asyncCall() {
-//     console.log('calling');
-//     const result = await resolveAfter2Seconds();
-//     console.log(result);
-//     // expected output: "resolved"
-//   }
