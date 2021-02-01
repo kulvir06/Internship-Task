5e26b27d6188e6eccaed3a4396865993559a872e
kulvir06 <56261797+kulvir06@users.noreply.github.com>
2021-02-01 23:13:25 +0530

    Update boardListsUpdate.js

--- user methods/boardListsUpdate.js
+++ user methods/boardListsUpdate.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 const dbconnect = require("./connection.js");
 
-var con =dbconnect.connection();
+const con =dbconnect.connection();
 
 module.exports={
-    update: function(lists,boardName){
-        var sql = "update boards set lists = "+mysql.escape(lists)+"where name = "+mysql.escape(boardName)+";";
+    update: (lists,boardName) => {
+        let sql = "update boards set lists = "+mysql.escape(lists)+"where name = "+mysql.escape(boardName)+";";
         con.query(sql,function(err,result,fields){
             if(err) throw err;
             
